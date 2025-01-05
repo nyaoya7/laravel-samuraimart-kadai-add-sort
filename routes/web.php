2848bded17dd61c7541f6048e9fe84b6bb56c131
nyaoya7 <n.yasugi@gmail.com>
2025-01-05 13:38:35 +0900

    #15 ソート機能まで実装完了

--- routes/web.php
+++ routes/web.php
@@ -33,8 +33,11 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-Route::resource('products', ProductController::class);
- 
-Route::post('reviews', [ReviewController::class, 'store'])->name('reviews.store');
-Route::post('favorites/{product_id}', [FavoriteController::class, 'store'])->name('favorites.store');
-Route::delete('favorites/{product_id}', [FavoriteController::class, 'destroy'])->name('favorites.destroy');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::resource('products', ProductController::class);
+
+    Route::post('reviews', [ReviewController::class, 'store'])->name('reviews.store');
+
+    Route::post('favorites/{product_id}', [FavoriteController::class, 'store'])->name('favorites.store');
+    Route::delete('favorites/{product_id}', [FavoriteController::class, 'destroy'])->name('favorites.destroy');
+});
